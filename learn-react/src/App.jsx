5c0fa74fcd802dfdb194d779c6ef8bb38a1c82c9
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2025-01-30 22:07:20 +0530

    Card Components

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 
 function App() {
     return(
-      <><Header />
-      <Food />      
-      <Footer />
+      <>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       </>
     );
 };
