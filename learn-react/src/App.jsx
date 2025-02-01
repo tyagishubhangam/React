8b1a5b8480637714e11f596fa507104214d94ae5
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2025-02-01 15:14:20 +0530

    implemented onChange event handler

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,8 +1,8 @@
-import Counter from "./Counter.jsx";
+import MyComponent from "./Mycomponent.jsx";
 function App() {
     return(
       <>
-      <Counter />
+      <MyComponent />
       </>
     );
 };
